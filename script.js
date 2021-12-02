e816b6cd75977997105f8bc226618cfe48e56a77
Nick Weiner <nicholas.weiner@icloud.com>
2021-12-01 21:14:50 -0500

    fixed indentation

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
-const generateTarget = (min, max) => {
-    return Math.floor(Math.random() * (9 - 0) + 0);
-  }
-
-  // Log Generate Target
-  console.log(generateTarget());
-
-const compareGuesses = (userGuess, comGuess, secretGuess) => {
-
-    if (Math.abs(userGuess - secretGuess) < Math.abs(comGuess - secretGuess)) {
-        return true
-    } else if (Math.abs(comGuess - secretGuess) < Math.abs(userGuess - secretGuess)) {
-        return false
-    } else {
-        return true
+    const generateTarget = (min, max) => {
+        return Math.floor(Math.random() * (9 - 0) + 0);
     }
-}
+
+    // Log Generate Target
+    console.log(generateTarget());
+
+    const compareGuesses = (userGuess, comGuess, secretGuess) => {
+
+        if (Math.abs(userGuess - secretGuess) < Math.abs(comGuess - secretGuess)) {
+            return true;
+        } else if (Math.abs(comGuess - secretGuess) < Math.abs(userGuess - secretGuess)) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
 
     // Log Compare Guesses
     console.log(compareGuesses(10, 10, 10));
+
