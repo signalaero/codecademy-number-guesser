bcbbbe77c9be6d119c98c3268bdfe08b8840767f
Nick Weiner <nicholas.weiner@icloud.com>
2021-12-01 20:39:53 -0500

    generateTarget() function - Task 3

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget(min, max) {
+    return Math.floor(Math.random() * (9 - 0) + 0);
+  }
+
+  // Log Generate Target
+  console.log(generateTarget());
