dda5a7b3289aafe7216ab1052201c9fd55609a5b
Nick Weiner <nicholas.weiner@icloud.com>
2021-12-01 21:53:28 -0500

    updateScore - Task 5

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+    // Task 3
     const generateTarget = (min, max) => {
         return Math.floor(Math.random() * (9 - 0) + 0);
     }
@@ -11,6 +12,7 @@ let currentRoundNumber = 1;
     // Log Generate Target
     console.log(generateTarget());
 
+    // Task 4
     const compareGuesses = (userGuess, comGuess, secretGuess) => {
 
         if (Math.abs(userGuess - secretGuess) < Math.abs(comGuess - secretGuess)) {
@@ -25,4 +27,15 @@ let currentRoundNumber = 1;
 
     // Log Compare Guesses
     console.log(compareGuesses(10, 10, 10));
+
+    
+    // Task 5
+    const updateScore = (winner) => {
+
+        if('human' || 'computer') {
+            winner++
+        }    
+    }
+
+
 
